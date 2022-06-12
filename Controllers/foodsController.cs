eac0e295b6558c9f63c0cce4eb26431a69bc8d72
bigjohn98pl-PC <56130048+bigjohn98pl@users.noreply.github.com>
2022-06-12 20:37:44 +0200

    Add new food controler

---
+++ Controllers/foodsController.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using FoodStockChecker.Data;
+using FoodStockChecker.Models;
+
+namespace FoodStockChecker.Controllers
+{
+    public class foodsController : Controller
+    {
+        private readonly FoodStockCheckerContext _context;
+
+        public foodsController(FoodStockCheckerContext context)
+        {
+            _context = context;
+        }
+
+        // GET: foods
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.food.ToListAsync());
+        }
+
+        // GET: foods/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var food = await _context.food
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (food == null)
+            {
+                return NotFound();
+            }
+
+            return View(food);
+        }
+
+        // GET: foods/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: foods/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,Category,Name,Wage,ExpireDate")] food food)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(food);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(food);
+        }
+
+        // GET: foods/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var food = await _context.food.FindAsync(id);
+            if (food == null)
+            {
+                return NotFound();
+            }
+            return View(food);
+        }
+
+        // POST: foods/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,Category,Name,Wage,ExpireDate")] food food)
+        {
+            if (id != food.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(food);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!foodExists(food.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(food);
+        }
+
+        // GET: foods/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var food = await _context.food
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (food == null)
+            {
+                return NotFound();
+            }
+
+            return View(food);
+        }
+
+        // POST: foods/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var food = await _context.food.FindAsync(id);
+            _context.food.Remove(food);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool foodExists(int id)
+        {
+            return _context.food.Any(e => e.Id == id);
+        }
+    }
+}
---
+++ Data/FoodStockCheckerContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using FoodStockChecker.Models;
+
+namespace FoodStockChecker.Data
+{
+    public class FoodStockCheckerContext : DbContext
+    {
+        public FoodStockCheckerContext (DbContextOptions<FoodStockCheckerContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<FoodStockChecker.Models.food> food { get; set; }
+    }
+}
