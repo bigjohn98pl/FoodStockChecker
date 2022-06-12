f80ba49038e360bdb6b1bb0c75b938aaaf3c6979
bigjohn98pl-PC <56130048+bigjohn98pl@users.noreply.github.com>
2022-06-12 20:22:02 +0200

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace FoodStockChecker.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
