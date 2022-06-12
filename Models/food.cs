52b438e9ce3c910b020a141b3fc28bb8c622f197
bigjohn98pl-PC <56130048+bigjohn98pl@users.noreply.github.com>
2022-06-12 20:33:00 +0200

    Create class food

---
+++ Models/food.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FoodStockChecker.Models
+{
+    public class food
+    {
+        public int Id { get; set; }
+        public string Category { get; set; }
+        public string Name { get; set; }
+        public double Wage { get; set; }
+        public  DateTime ExpireDate { get; set; }
+
+    }
+}
